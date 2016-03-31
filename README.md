a8418da7033cfbc4c67b57cfa06ca291541db899
bli423 <bli423@naver.com>
2016-03-31 17:04:19 +0900

    Added files via upload

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-# team07_week4
-team07_week4
+# sampleweb
 
-실전코딩 7조 4주차
-
-<h1>메이븐 goal로 package 수행 후 실행</h1>
-
-실행방식
-
+This is a sample web project for lecturing.
---
+++ src/main/java/com/your/dream/web/Dream.java
@@ -0,0 +1,12 @@
+package com.your.dream.web;
+
+public class Dream {
+
+	public Dream(int ageToBeTrue) {
+	}
+
+	public boolean comeTrue() {
+		return false;
+	}
+
+}
