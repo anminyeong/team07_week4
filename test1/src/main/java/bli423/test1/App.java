141c798b3eecd746d71558ef916ef18d4b0be0fe
nam1 <nam1@nam>
2016-04-01 17:55:24 +0900

    c

--- test1/src/main/java/bli423/test1/App.java
+++ test1/src/main/java/bli423/test1/App.java
@@ -1,5 +1,8 @@
 package bli423.test1;
 
+import java.util.Scanner;
+
+import test_hw.Calculate;
 import test_hw.Run;
 
 /**
@@ -8,13 +11,26 @@ import test_hw.Run;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-    	try{
-    		new Run(); 
-    	}
-    	catch(Exception e){
-    		throw new IllegalArgumentException("run error");
-    	}
-    }
+	private void main(){		
+	}	
+	private static void run(){
+		Scanner aScanner = new Scanner(System.in);
+		String name;
+		int minute;
+		int line;
+		double bill;
+		System.out.println("plan을 입력해주세요.");
+		name = aScanner.next();
+		System.out.println("총 사용시간을 입력해주세요.");
+		minute = aScanner.nextInt();
+		System.out.println("사용회선 수를 입력해주세요.");
+		line = aScanner.nextInt();
+		Calculate aCalculate = new Calculate(name,minute,line);
+		bill = aCalculate.calculate();
+		System.out.println("사용한 금액은:"+bill+"입니다.");
+	}
+	public static void main( String[] args )
+	{
+		run();
+	}
 }
