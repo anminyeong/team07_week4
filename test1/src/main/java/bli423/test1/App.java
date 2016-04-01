3898ee294f36e3745ef9b701d21770445018121a
nam1 <nam1@nam>
2016-04-01 18:18:06 +0900

    update

--- test1/src/main/java/bli423/test1/App.java
+++ test1/src/main/java/bli423/test1/App.java
@@ -1,6 +1,7 @@
 package bli423.test1;
 
 import java.util.Scanner;
+import java.util.logging.*;
 
 import test_hw.Calculate;
 import test_hw.Run;
@@ -11,23 +12,25 @@ import test_hw.Run;
  */
 public class App 
 {
-	private void main(){		
+	private void main(){	
+		//this nothing
 	}	
 	private static void run(){
+		Logger logger = Logger.getLogger(App.class.getName());
 		Scanner aScanner = new Scanner(System.in);
 		String name;
 		int minute;
 		int line;
 		double bill;
-		System.out.println("plan을 입력해주세요.");
+		logger.log(Level.INFO,"plan을 입력해주세요.");
 		name = aScanner.next();
-		System.out.println("총 사용시간을 입력해주세요.");
+		logger.log(Level.INFO,"총 사용시간을 입력해주세요.");
 		minute = aScanner.nextInt();
-		System.out.println("사용회선 수를 입력해주세요.");
+		logger.log(Level.INFO,"사용회선 수를 입력해주세요.");
 		line = aScanner.nextInt();
 		Calculate aCalculate = new Calculate(name,minute,line);
 		bill = aCalculate.calculate();
-		System.out.println("사용한 금액은:"+bill+"입니다.");
+		logger.log(Level.INFO,"사용한 금액은:"+bill+"입니다.");
 	}
 	public static void main( String[] args )
 	{
